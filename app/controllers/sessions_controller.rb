f511d18e1a81ca62ba47e765989c1918c6d27159
Haloren <mckennakyler@gmail.com>
2020-08-15 16:06:40 -0700

    started sessions create, added error messages to login & signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,11 +9,19 @@ class SessionsController < ApplicationController
     end
 
     def new
-
     end
 
     def create
-        
+        # byebug
+        user = User.find_by(email: params[:user][:email])
+
+        if user.try(:authenticate, params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else    
+            flash[:message] = "Incorrect Email and/or Password"
+            redirect_to login_path
+        end
     end
 
 end
