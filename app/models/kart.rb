ec549844cf7ed37d4a2806bee79dcd08bcdad738
Haloren <mckennakyler@gmail.com>
2020-08-15 16:56:44 -0700

    Build a  Kart form

--- app/models/kart.rb
+++ app/models/kart.rb
@@ -4,4 +4,10 @@ class Kart < ApplicationRecord
 
   has_many :reviews
   has_many :users, through: :reviews
+
+  DRIVERS = ['Driver1', 'Driver2']
+  COLORS = ['Red', 'Blue', 'Purple']
+  BODIES = ['Body1', 'Body2']
+  WHEELS = ['Wheels1', 'Wheels2']
+
 end
