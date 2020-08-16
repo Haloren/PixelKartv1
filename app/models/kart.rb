786e48982a32f85dcf0ee7a55d05645fe4f5de46
Haloren <mckennakyler@gmail.com>
2020-08-15 18:42:06 -0700

    added validations for kart model

--- app/models/kart.rb
+++ app/models/kart.rb
@@ -5,13 +5,16 @@ class Kart < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
 
-  
+  accepts_nested_attributes_for :garage
+
+  validates :driver, presence: true
+  validates :color, presence: true
+  validates :body, presence: true
+  validates :wheels, presence: true
 
   DRIVERS = ['Driver1', 'Driver2']
   COLORS = ['Red', 'Blue', 'Purple']
   BODIES = ['Body1', 'Body2']
   WHEELS = ['Wheels1', 'Wheels2']
-
-  accepts_nested_attributes_for :garage
 
 end
